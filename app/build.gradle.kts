ab3b7b1edfd6e0baf8d6a395dd68ed8a6ee047d3
Marco Salsiccia <marco.salsiccia@gmail.com>
2026-06-17 11:22:10 -0700

    Updated version code and number, prep for play store release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.marconius.WordBopper"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.3.0"
+        versionCode = 6
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
